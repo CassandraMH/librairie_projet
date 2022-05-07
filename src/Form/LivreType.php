f1e5bf9f65fec392f84980fb19e8ddb397df47b6
CassandraM-H <c.muller-heyob@outlook.fr>
2022-05-07 15:02:13 +0200

    Create LivreType.php

---
+++ src/Form/LivreType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Livre;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
+
+class LivreType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('titre')
+            ->add('auteur')
+            ->add('description')
+            ->add('prix')
+            ->add('image', UrlType::class, [
+                'attr'=>[
+                    'class'=>'#coupcoeur .tabs-content img'],
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Livre::class,
+        ]);
+    }
+}
